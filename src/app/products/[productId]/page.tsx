c26b5ef9ea93a16a143c7b7310eb08819c424676
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-07-19 01:40:35 +0600

    First Commit

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DynamicProductPage = () => {
+  return (
+    <div>
+      <h1>Dynamic Product Page</h1>
+    </div>
+  );
+};
+
+export default DynamicProductPage;
